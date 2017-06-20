1b04c2a30cff7e91be4972b5fc16c563d26d6e01
Kristijan505 <kristijan.pravica@gmail.com>
2017-06-20 15:48:42 +0200

    Admin panel

--- ServisVozila/ServisVozila/Controllers/AdminController.cs
+++ ServisVozila/ServisVozila/Controllers/AdminController.cs
@@ -33,29 +33,103 @@ namespace ServisVozila.Controllers
             }
         }
 
-        public ContentResult DodajRoluKorisniku()
+        public ActionResult DodajRoluKorisniku(string korisnici, string dali)
         {
             using (ApplicationDbContext context = new ApplicationDbContext())
             {
-                string rola = "admin";
-                string korisnik = @"kristijan.pravica@gmail.com";
-                IdentityResult result;
-                var store = new UserStore<ApplicationUser>(context);
-                var manager = new UserManager<ApplicationUser>(store);
-                string userid = context.Users.FirstOrDefault(s => s.UserName == korisnik).Id;
-                result = manager.AddToRole(userid, rola);
-                if (result.Succeeded)
-                    return Content("Korisniku " + korisnik + " dodana rola " + rola);
+                if (dali == "DA" && korisnici != "Odaberite korisnika")
+                {
+                    string rola = "admin";
+                    IdentityResult result;
+                    var store = new UserStore<ApplicationUser>(context);
+                    var manager = new UserManager<ApplicationUser>(store);
+                    string userid = context.Users.First(s => s.UserName == korisnici).Id;
+                    result = manager.AddToRole(userid, rola);
+                    if (result.Succeeded)
+                    {
+                        string porukaZaRolu = "Korisniku " + korisnici + " je dodana rola " + rola;
+                        ViewBag.porukaZaRolu = porukaZaRolu;
+                        return View();
+                    }
+                    else
+                    {
+                        string porukaZaRolu = "Greška prilikom dodavanja role " + rola + " korisniku " + korisnici;
+                        ViewBag.porukaZaRolu = porukaZaRolu;
+                        return View();
+                    }
+                }
+                else if (korisnici == "Odaberite korisnika" && dali == "DA")
+                {
+                    string porukaZaRolu = "Niste odabrali korisnika.";
+                    ViewBag.porukaZaRolu = porukaZaRolu;
+                    return View();
+                }
+                else if (korisnici != "Odaberite korisnika" && dali == null)
+                {
+                    string porukaZaRolu = "Niste postavili kvačicu.";
+                    ViewBag.porukaZaRolu = porukaZaRolu;
+                    return View();
+                }
                 else
-                    return Content("Greška prilikom dodavanja role " + rola + " korisniku " + korisnik);
+                {
+                    string porukaZaRolu = "Niste niti odabrali korisnika niti postavili kvačicu.";
+                    ViewBag.porukaZaRolu = porukaZaRolu;
+                    return View();
+                }
+            }
+        }
+        public ActionResult SkiniRoluAdminu(string admini, string dali)
+        {
+            using (ApplicationDbContext context = new ApplicationDbContext())
+            {
+                if (dali == "DA" && admini != "Odaberite admina")
+                {
+                    string rola = "admin";
+                    IdentityResult result;
+                    var store = new UserStore<ApplicationUser>(context);
+                    var manager = new UserManager<ApplicationUser>(store);
+                    string userid = context.Users.First(s => s.UserName == admini).Id;
+                    result = manager.AddToRole(userid, rola);
+                    if (result.Succeeded)
+                    {
+                        string porukaZaRolu = "Adminu " + admini + " su skinuta admin prava.";
+                        ViewBag.porukaZaRolu = porukaZaRolu;
+                        return View();
+                    }
+                    else
+                    {
+                        string porukaZaRolu = "Greška prilikom skidanja admin prava adminu " + admini + ".";
+                        ViewBag.porukaZaRolu = porukaZaRolu;
+                        return View();
+                    }
+                }
+                else if (admini == "Odaberite admina" && dali == "DA")
+                {
+                    string porukaZaRolu = "Niste odabrali admina.";
+                    ViewBag.porukaZaRolu = porukaZaRolu;
+                    return View();
+                }
+                else if (admini != "Odaberite admina" && dali == null)
+                {
+                    string porukaZaRolu = "Niste postavili kvačicu.";
+                    ViewBag.porukaZaRolu = porukaZaRolu;
+                    return View();
+                }
+                else
+                {
+                    string porukaZaRolu = "Niste niti odabrali admina niti postavili kvačicu.";
+                    ViewBag.porukaZaRolu = porukaZaRolu;
+                    return View();
+                }
             }
         }
         public ActionResult Prava()
         {
-            ApplicationDbContext admini = new ApplicationDbContext();
-            ViewBag.admini = admini.Users.Where(x => x.Roles.Select(role => role.RoleId).Contains("bab046ae-8c4c-44b2-9cd9-94022e15a6f8")).ToList();
-            ApplicationDbContext korisnici = new ApplicationDbContext();
-            ViewBag.korisnici = korisnici.Users.ToList();
+            ApplicationDbContext db = new ApplicationDbContext();
+            ViewBag.admini = db.Users.Where(x => x.Roles.Select(role => role.RoleId).Contains("bab046ae-8c4c-44b2-9cd9-94022e15a6f8")).ToList();
+            ViewBag.korisnici = db.Users.Where(x => !x.Roles.Select(role => role.RoleId).Contains("bab046ae-8c4c-44b2-9cd9-94022e15a6f8")).ToList();
+            //ApplicationDbContext korisnici = new ApplicationDbContext();
+            //ViewBag.korisnici = korisnici.Users.ToList();
             return View();
         }
     }
