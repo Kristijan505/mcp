4a1e684fe7bf88afe67e9b036dca1c69999a61f9
Kristijan505 <kristijan.pravica@gmail.com>
2017-06-23 23:30:51 +0200

    Malo preuređenje

--- ServisVozila/BazaZaEventZaKalendar.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE Events (
-    [id] INT IDENTITY (1, 1) NOT NULL,
-    [text] nvarchar (250) NULL,
-    [start_date] datetime NOT NULL,
-    [end_date] datetime NOT NULL,
-    PRIMARY KEY (id)
-)
